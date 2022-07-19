20f7f8e5730f850e8908032be2f3a243b86d4a70
Roman <nick@MacBook-Air-Roman.local>
2022-07-19 14:33:49 +0300

    Fixing button time

--- btn_classes.py
+++ btn_classes.py
@@ -29,7 +29,6 @@ class Notify:
     async def turn_on_notif(self, bot, message):
         markup = types.InlineKeyboardMarkup(row_width=6)
         times = []
-        markup.add(types.KeyboardButton('/help'))
         for time in [f'0{i}:00' if i < 10 else f'{i}:00' for i in range(0, 24)]:
             times.append(types.InlineKeyboardButton(text=time, callback_data=time))
         markup.add(*(i for i in times))
